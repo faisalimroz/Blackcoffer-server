488e244661c56cbbce8b167f62be874fe4d73901
Faisal Imroz <faisalimroz49@gmail.com>
2024-02-22 02:44:18 +0600

    second commit

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const app = express();
+const jwt = require('jsonwebtoken');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
+
+const port = process.env.PORT || 5000;
+const cors = require('cors');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tort7uo.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+app.use(cors());
+app.use(express.json());
+
+
+async function run() {
+  try {
+    await client.connect();
+    const dataCollection = client.db('reactchart').collection('data');
+    
+
+    app.get('/data', async (req, res) => {
+      try {
+        const economicInfo = await dataCollection.find().toArray();
+     
+        res.json(economicInfo);
+      } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Failed to retrieve economicInfo' });
+      }
+    });
+    
+  } finally {
+  }
+}
+
+run().catch(console.log);
+
+
+app.listen(port, () => {
+  console.log(`server is running on PORT: ${port}`);
+});
